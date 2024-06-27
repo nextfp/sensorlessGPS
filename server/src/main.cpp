70cf8a602bd668ee247880da9585b59a435b4254
labFish <labFish893@gmail.com>
2024-06-27 20:00:06 +0900

    feat: スピード機能の追加とM5Stack用に変更

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <M5Unified.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
@@ -18,18 +19,42 @@ class MyServerCallbacks : public BLEServerCallbacks
   void onConnect(BLEServer *pServer)
   {
     deviceConnected = true;
-    Serial.println("Connected");
+    // Serial.println("Connected");
   };
 
   void onDisconnect(BLEServer *pServer)
   {
     deviceConnected = false;
-    Serial.println("Disconnected");
+    // Serial.println("Disconnected");
   }
 };
 
 class MyCallbacks : public BLECharacteristicCallbacks
 {
+  String geolocation[3];
+
+  void splitString(String input)
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      geolocation[i] = "";
+    }
+
+    int currentIndex = 0;
+    int startIndex = 0;
+    int endIndex = input.indexOf(',');
+
+    while (endIndex != -1)
+    {
+      geolocation[currentIndex++] = input.substring(startIndex, endIndex);
+      startIndex = endIndex + 1;
+      endIndex = input.indexOf(',', startIndex);
+    }
+
+    // 最後の区切り文字の後の部分を追加
+    geolocation[currentIndex++] = input.substring(startIndex);
+  }
+
   void onWrite(BLECharacteristic *pCharacteristic)
   {
     std::string value = pCharacteristic->getValue();
@@ -37,14 +62,30 @@ class MyCallbacks : public BLECharacteristicCallbacks
     if (value.length() > 0)
     {
       String message = value.c_str();
-      Serial.println(message);
+      // Serial.println("Received: " + message);
+      splitString(message);
+      for (int i = 0; i < 3; i++)
+      {
+        // Serial.println(geolocation[i]);
+      }
+      M5.Lcd.setCursor(10, 10);
+      M5.Lcd.println("緯度:" + geolocation[0]);
+      M5.Lcd.setCursor(10, 60);
+      M5.Lcd.println("経度:" + geolocation[1]);
+      M5.Lcd.setCursor(10, 110);
+      M5.Lcd.println("速度:" + geolocation[2]);
     }
   }
 };
 
 void setup()
 {
-  Serial.begin(115200);
+  M5.begin();
+  M5.Power.begin();
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextColor(WHITE, BLACK);
+  M5.Lcd.setTextSize(4);
+  // Serial.begin(115200);
 
   BLEDevice::init("ESP32_BLE_SERVER"); // この名前がスマホなどに表示される
   BLEServer *pServer = BLEDevice::createServer();
@@ -67,7 +108,7 @@ void setup()
   pAdvertising->setMinPreferred(0x06); // iPhone接続の問題に役立つ
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
-  Serial.println("Characteristic defined! Now you can read it in your phone!");
+  // Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop()
@@ -76,7 +117,7 @@ void loop()
   if (deviceConnected)
   {
     // データを送信
-    Serial.println("Sending data");
+    // Serial.println("Sending data");
     String str(millis() / 1000);
     pTxCharacteristic->setValue(str.c_str());
     pTxCharacteristic->notify();
